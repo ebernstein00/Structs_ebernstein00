7912a6d5a518300565feed345db5bd8ef1d18857
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-15 15:32:07 -0400

    Added modify function

--- struct.c
+++ struct.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <header.h>
 
-struct {int a; char surname[30];} player;
 
-int printer(struct {int a; char surname[30];} player){
-    printf("Number: %d\tName: %s", player.a, player.surname);
+
+int printer(struct player player){
+    printf("Number: %d\tName: %s\n", player.a, player.surname);
     return 1;
+}
+
+struct player modify(struct player player, int x, char name[30]){
+    player.a = x;
+    player.surname = name;
+    return player;
 }
