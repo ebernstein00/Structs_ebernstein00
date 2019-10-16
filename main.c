5a36683010b3b2fc9db5cf5f2e0d7e67ce3dc4a4
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-16 15:10:53 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <header.h>
+#include "header.h"
+
+struct s {
+    int a;
+    char surname[30];
+};
 
 int main(){
     struct s player = example();
