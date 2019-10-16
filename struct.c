0c527ad6a4e525e82ec29bc6cabaee649841816a
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-15 20:37:23 -0400

    Updated all methods

--- struct.c
+++ struct.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <header.h>
 
-struct player example(){
-    return struct
+struct s example(){
+    struct s ans;
+    (*ans).a = rand();
+    (*ans).surname = rand();
+    return ans;
+}
 
 int printer(struct s player){
     printf("Number: %d\tName: %s\n", player.a, player.surname);
     return 1;
 }
 
-struct player modify(struct s player, int x, char name[30]){
-    player.a = x;
-    player.surname = name;
-    return player;
+void modify(struct s* player, int x, char name[30]){
+    player->a = x;
+    player->surname = name;
 }
