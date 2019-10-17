ca512ab6bfbc1f5b10704749de60c7375afd624e
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-10-16 20:08:27 -0400

    Update header.h

--- header.h
+++ header.h
@@ -1,4 +1,4 @@
 //struct {int a; char surname[30];} s;
 struct s example();
 void printer(struct s player);
-void modify(struct s* player, int x, char* name);
+void modify(struct s* player, int x, char name[30]);
